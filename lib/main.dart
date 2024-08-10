60ab5cbadef649a439453f607386629811e34689
moobin <moobinislam3@gmail.com>
2024-08-10 08:41:01 +0600

    dart tow

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 
+import 'drwar/drawer.dart';
+
 void main() {
   runApp(const Myapp());
 }
@@ -13,48 +15,35 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.cyan,
-          // leading:
-          //     Icon(Icons.account_balance_wallet_sharp, color: Colors.white),
-          // leadingWidth: 40,
-        ),
-        drawer: Drawer(
-          backgroundColor: Colors.white.withOpacity(0.2),
-          elevation: 40,
-          shadowColor: Colors.red,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('data'),
-              Text('data'),
-              Text('data'),
-            ],
-          ),
-          width: 200,
-        ),
+        appBar: AppBar(),
+        drawer: DrawerSlid(),
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg'),
-                  fit: BoxFit.cover),
-              shape: BoxShape.circle,
-              border: GradientBoxBorder(
-                width: 5,
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  colors: [
-                    Colors.yellow,
-                    Colors.red,
-                    Colors.black,
-                  ],
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg'),
+                      fit: BoxFit.cover),
+                  shape: BoxShape.circle,
+                  border: GradientBoxBorder(
+                    width: 5,
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      colors: [
+                        Colors.yellow,
+                        Colors.red,
+                        Colors.black,
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 10),
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
