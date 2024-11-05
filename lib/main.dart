3f3f5d088f20f4ef629a2852d4217658cf890b81
moobin <moobinislam3@gmail.com>
2024-11-05 10:36:40 +0600

    cc

--- lib/main.dart
+++ lib/main.dart
@@ -1,215 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/YouTub/youtubtest.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(
+    MyApp(), // Wrap your app
+  );
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Youtubtest(),
-
-      ///class test
-      // Home(),
+      home: Home(),
     );
   }
 }
-/**
+
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
-  State<StatefulWidget> createState() => Screan();
+  State<Home> createState() => _HomeState();
 }
 
-class Screan extends State<Home> {
-  final TextEditingController _one = TextEditingController();
-  final TextEditingController _tow = TextEditingController();
-  final GlobalKey<FormState> _formkye = GlobalKey<FormState>();
-  double _result = 0;
-  //counter
-  int counter = 0;
-
-  //end counter
-  //snacbar
-  Snacbar(sms, context) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(sms)));
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: const Text('Home'), centerTitle: true),
-      body: Padding(
-        padding: EdgeInsets.all(22),
-        child: Form(
-          key: _formkye,
-          child: Column(
-            children: [
-              TextFormField(
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Input kye';
-                  }
-                  return null;
-                },
-                controller: _one,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text('Input'),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Input kye';
-                  } else {
-                    return null;
-                  }
-                },
-                controller: _tow,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text('Input'),
-                ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  OutlinedButton(onPressed: add, child: Text('+')),
-                  OutlinedButton(onPressed: remove, child: Text('-')),
-                  OutlinedButton(onPressed: into, child: Text('*')),
-                  OutlinedButton(onPressed: divider, child: Text('/')),
-                ],
-              ),
-              SizedBox(height: 20),
-              Text('Resul : $_result')
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text('Home Screen'),
       ),
-      // body: Center(child: Text('Counter number $counter')),
-      // floatingActionButton: buildFloatingActionButton(context),
+      body: LayoutBuilder(
+
+        builder: (context, Container) {
+          final width = Container.maxWidth;
+          final hight = Container.maxHeight;
+          if (Container.maxWidth < 600) {
+            return Text('Mobile Screen');
+          }
+          if () {
+            return Text('Tablet Screen');
+          } else {
+            return Text('Desktop Screen');
+          }
+        },
+      ),
     );
   }
-
-  FloatingActionButton buildFloatingActionButton(BuildContext context) {
-    return FloatingActionButton(
-        onPressed: () {
-          counter++;
-          setState(() {
-            Snacbar('sms', context);
-          });
-        },
-        child: Icon(Icons.add));
-  }
-
-  void add() {
-    if (_formkye.currentState!.validate()) {
-      double one = double.tryParse(_one.text) ?? 0;
-      double tow = double.tryParse(_tow.text) ?? 0;
-      _result = one + tow;
-      setState(() {});
-    }
-  }
-
-  void remove() {
-    if (_formkye.currentState!.validate()) {
-      double one = double.tryParse(_one.text) ?? 0;
-      double tow = double.tryParse(_tow.text) ?? 0;
-      _result = one - tow;
-      setState(() {});
-    }
-  }
-
-  void into() {
-    if (_formkye.currentState!.validate()) {
-      double one = double.tryParse(_one.text) ?? 0;
-      double tow = double.tryParse(_tow.text) ?? 0;
-      _result = one * tow;
-      setState(() {});
-    }
-  }
-
-  void divider() {
-    if (_formkye.currentState!.validate()) {
-      double one = double.tryParse(_one.text) ?? 0;
-      double tow = double.tryParse(_tow.text) ?? 0;
-      _result = one / tow;
-      setState(() {});
-    }
-  }
-
-  bool valid() {
-    if (_one.text.isEmpty) {
-      return false;
-    } else if (_tow.text.isEmpty) {
-      return false;
-    } else {
-      return true;
-    }
-  }
 }
-
-// class Home extends StatefulWidget {
-//   const Home({super.key});
-//
-//   @override
-//   State<Home> createState() => _HomeState();
-// }
-//
-// class _HomeState extends State<Home> {
-//   ///counter number
-//   int one = 0;
-//
-//   MySnac(sms, context) {
-//     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(sms)));
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Home'),
-//       ),
-//       body: Center(
-//         child: Text('Counter $one'),
-//       ),
-//       floatingActionButton: Row(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           FloatingActionButton(
-//             onPressed: () {
-//               one--;
-//               print(one);
-//               setState(() {
-//                 MySnac('$one', context);
-//               });
-//             },
-//             child: Icon(Icons.remove),
-//           ),
-//           SizedBox(width: 10),
-//           FloatingActionButton(
-//             onPressed: () {
-//               one++;
-//               print(one);
-//               setState(() {
-//                 MySnac('$one', context);
-//               });
-//             },
-//             child: Icon(Icons.add),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
- */
